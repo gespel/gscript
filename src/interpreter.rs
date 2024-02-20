fb2dd014e586d7b42fd23c6e1f7836b62b9da014
gespel <sten.heimbrodt@hotmail.de>
2024-02-21 00:09:06 +0100

    wyld

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,46 +1,85 @@
+use std::collections::HashMap;
+use std::process::exit;
 use crate::lexer::Token;
 
 pub struct Interpreter {
     tokens: Vec<Token>,
-    index: usize,
+    line_index: usize,
+    functions: HashMap<String, Vec<Token>>,
+    variables: HashMap<String, f64>
 }
 
 impl Interpreter {
     pub fn new(tokens: Vec<Token>) -> Interpreter {
         Interpreter {
             tokens,
-            index: 0
-        }
-    }
-    pub fn interpret(&mut self) {
-        let mut line: Vec<Token> = Vec::new();
-        for token in self.tokens.clone() {
-            if token == Token::EOL {
-                self.interpret_line(line.clone());
-                line.clear();
-                continue;
-            }
-            line.push(token);
+            line_index: 0,
+            functions: HashMap::new(),
+            variables: HashMap::new()
         }
     }
 
-    fn interpret_line(&mut self, token_line: Vec<Token>) {
-        println!("Interpreting line now!");
-        let mut i = 0;
-        if let Token::Identifier(ident) = token_line[i].clone() {
-            i += 1;
+    pub fn interpret(&mut self) {
+        if let Token::Identifier(ident) = self.tokens[self.line_index].clone() {
+            self.line_index += 1;
             match ident.as_str() {
                 "fn" => {
-                    if let Token::Identifier(fn_name) = token_line[i].clone() {
-
+                    if let Token::Identifier(fn_name) = self.tokens[self.line_index].clone() {
+                        self.line_index += 1;
+                        self.eat(Token::LeftParen);
+                        self.eat(Token::RightParen);
+                        self.eat(Token::LeftBrack);
+                        let mut fn_tokens: Vec<Token> = Vec::new();
+                        loop {
+                            fn_tokens.push(self.tokens[self.line_index].clone());
+                            if let Token::RightBrack = self.tokens[self.line_index+1] {
+                                break;
+                            }
+                            self.line_index += 1;
+                        }
+                        self.line_index += 1;
+                        self.functions.insert(fn_name, fn_tokens);
                     }
-
                 }
-                _ => {
+                var_name => {
+                    if let Token::Assign = self.tokens[self.line_index].clone() {
+                        self.line_index += 1;
+                        if let Token::Number(var_value) = self.tokens[self.line_index].clone() {
+                            self.variables.insert(var_name.parse().unwrap(), var_value);
+                        }
+                        if let Token::Identifier(var2_name) = self.tokens[self.line_index].clone() {
+                            self.line_index += 1;
+                            self.eat(Token::Plus);
+                            if let Token::Number(add) = self.tokens[self.line_index].clone() {
+                                self.line_index += 1;
+                                self.variables.insert(var_name.parse().unwrap(), self.variables.get(&var2_name).unwrap().clone() + add);
+                            }
 
+                        }
+                    }
                 }
             }
         }
+        self.line_index += 1;
+        if self.line_index < self.tokens.len() - 1 {
+            self.interpret();
+        }
+    }
 
+    fn eat(&mut self, t: Token) {
+        if self.tokens[self.line_index].clone() == t {
+            self.line_index += 1;
+        }
+        else {
+            println!("SYNTAX ERRROR!");
+            exit(-1);
+        }
+    }
+
+    pub fn print_debug(&self) {
+        println!("defined functions:");
+        println!("{:?}", self.functions);
+        println!("defined variables:");
+        println!("{:?}", self.variables);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,20 @@ mod lexer;
 mod interpreter;
 
 fn main() {
-    let mut l = Lexer::new("xasd = 3;xa = 2;s = 1;");
+    let mut l = Lexer::new("
+        y2 = 2
+        y3 = 5
+        fn test() {
+            x = 0
+            y = 1
+        }
+        fn test2() {
+            x1 = 2
+        }
+        y2 = y3 + 3
+    ");
+    //println!("{:?}", l.tokenize());
     let mut i = Interpreter::new(l.tokenize());
     i.interpret();
+    i.print_debug();
 }
